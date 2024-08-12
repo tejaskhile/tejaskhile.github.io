497d0e918b72f4e7104e30c6aac0806ba8abad3e
Tejas Khile <128159889+tejaskhile@users.noreply.github.com>
2024-08-12 13:49:00 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
           <div className='media'>
             <div className="social-media">
               <a href="https://github.com/tejaskhile"><i className="fa-brands fa-github"></i></a>
-              <a href="/"><i className="fa-brands fa-linkedin"></i></a>  
+              <a href="https://www.linkedin.com/in/tejas-khile-b0a45025a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i className="fa-brands fa-linkedin"></i></a>  
             </div>
 
             {/* <button id='resume-btn'>
