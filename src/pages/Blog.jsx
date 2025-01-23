2f8153b204e005a2ba09be03cadacfbf71bbce1b
tejaskhile <tejaskhile07@gmail.com>
2025-01-23 16:25:57 +0530

    Updated blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
   return (
     <div className='blog'>
       <BlogPost
-      name="My Experience with Front-End Programming"
+      name="My Experience with Front-End Programming."
       date="20 Jan"
       desc1="I was amazed by how websites could transform concepts into interactive experiences when I first began learning to code. I started by making basic static pages using HTML, CSS, and JavaScript, then I progressively added interactivity."
       desc2="My projects reached new heights as I experimented with frameworks like React and Vue.js. I learned how to handle routing, manage state, and create scalable web apps thanks to these technologies."
