50341a5ed9704d8157934429798a8253b2418570
tejaskhile <tejaskhile07@gmail.com>
2025-06-22 20:09:05 +0530

    latest commits

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,35 +1,46 @@
 const allprojects = [
-    {
-        name: "AI Chat App",
-        caption: "An intelligent chat app that feels like ChatGPT in your conversations. Just mention @ai to get instant AI-powered answers, code solutions, or helpful suggestions—right within your chat thread!",
-        link: "https://tejaskhile.github.io/chat-app/",
-        date: "May 2025"
-      },
-    {
-      name: "",
-      caption: "My Portfolio is now live! Explore my work and achievements. Your feedback is most welcome, checkout now!",
-      link: "/",
-      date: "Jan 2025"
-    },
-    {
-      name: "Codepen-project",
-      caption: "Built a CodePen clone with React! Live HTML, CSS, and JS editing with real-time preview. Perfecting the art of interactive coding environments.<br/> #ReactJS #WebDevelopment",
-      link: "https://tejaskhile.github.io/codepen-clone/",
-      date: "Dec 2023"
-    },
-    {
-      name: "Tic Tac Toe",
-      caption: "This application effectively demonstrates React's capabilities and offers an interactive, responsive user experience. #WebDevelopment",
-      link: "https://tic-tac-toe-one-delta.vercel.app/",
-      date: "Oct 2023"
-    },
-    {
-      name: "Music Player",
-      caption: "This music player was created using HTML, CSS and JavaScript. The music player features a modern design, with a user-friendly interface that allows users to easily control their music. #WebDevelopment",
-      link: "https://music-player-gray-seven.vercel.app/",
-      date: "Oct 2023"
-    }
-  ];
-  
-  export default allprojects;
-  
+  {
+    name: "Rental App",
+    caption:
+      "StayFinder is a full-stack room rental web app that allows users to browse listings, book stays, and manage properties with a clean, user-friendly interface.",
+    link: "https://tejaskhile.github.io/rental-app/",
+    date: "Jun 2025",
+  },
+  {
+    name: "AI Chat App",
+    caption:
+      "An intelligent chat app that feels like ChatGPT in your conversations. Just mention @ai to get instant AI-powered answers, code solutions, or helpful suggestions—right within your chat thread!",
+    link: "https://tejaskhile.github.io/chat-app/",
+    date: "May 2025",
+  },
+  {
+    name: "",
+    caption:
+      "My Portfolio is now live! Explore my work and achievements. Your feedback is most welcome, checkout now!",
+    link: "/",
+    date: "Jan 2025",
+  },
+  {
+    name: "Codepen-project",
+    caption:
+      "Built a CodePen clone with React! Live HTML, CSS, and JS editing with real-time preview. Perfecting the art of interactive coding environments.<br/> #ReactJS #WebDevelopment",
+    link: "https://tejaskhile.github.io/codepen-clone/",
+    date: "Dec 2023",
+  },
+  {
+    name: "Tic Tac Toe",
+    caption:
+      "This application effectively demonstrates React's capabilities and offers an interactive, responsive user experience. #WebDevelopment",
+    link: "https://tic-tac-toe-one-delta.vercel.app/",
+    date: "Oct 2023",
+  },
+  {
+    name: "Music Player",
+    caption:
+      "This music player was created using HTML, CSS and JavaScript. The music player features a modern design, with a user-friendly interface that allows users to easily control their music. #WebDevelopment",
+    link: "https://music-player-gray-seven.vercel.app/",
+    date: "Oct 2023",
+  },
+];
+
+export default allprojects;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,82 +1,105 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import '../styles/Home.css'
-import photo from "../assets/profilelogo.png"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Home.css";
+import photo from "../assets/profilelogo.png";
 // import bg from '../assets/bg.jpeg'
-import { useLocation } from 'react-router-dom'
-import Animation from '../components/Animation'
-
+import { useLocation } from "react-router-dom";
+import Animation from "../components/Animation";
 
 const Home = () => {
-
   const location = useLocation();
 
   return (
     <div className="home-content">
       <div className="first-section">
-        <div id='animation' className='bg-video'>
+        <div id="animation" className="bg-video">
           {/* <img src={bg} alt="img" /> */}
-          <Animation/>
+          <Animation />
         </div>
-        <div className='photo'>
-          <img src={photo} alt='' id='photo-div'></img>
-          <a className='flw-btn' href="https://www.instagram.com/tejaskhile_">Follow<i className="fa-brands fa-instagram"></i></a> 
+        <div className="photo">
+          <img src={photo} alt="" id="photo-div"></img>
+          <a className="flw-btn" href="https://www.instagram.com/tejaskhile_">
+            Follow<i className="fa-brands fa-instagram"></i>
+          </a>
           {/* {clicked && <i className="like-anime fa-solid fa-heart"></i>} */}
         </div>
       </div>
 
-      <div className='second-third-section'>
+      <div className="second-third-section">
         <div className="second-section">
           <h1>Tejas Khile</h1>
-          <h4>Crafting seamless digital experiences, one line of code at a time.</h4>
+          <h4>
+            Crafting seamless digital experiences, one line of code at a time.
+          </h4>
           <p>Full Stack Web Developer, Dreamer.</p>
         </div>
 
-        <div className='second-mid-section'>
-          <div className='info-content'>
+        <div className="second-mid-section">
+          <div className="info-content">
             <i className="icons fa-solid fa-briefcase"></i>
             <h5>Available</h5>
           </div>
-          <div className='info-content'>
+          <div className="info-content">
             <i className="icons fa-solid fa-cake-candles"></i>
             <h5>8th Oct</h5>
           </div>
-          <div className='info-content'>
-           <i className="icons fa-solid fa-calendar-days"></i>
-           <h5>Joined Jan 2025</h5>
+          <div className="info-content">
+            <i className="icons fa-solid fa-calendar-days"></i>
+            <h5>Joined Jan 2025</h5>
           </div>
-          <div className='info-content'>
+          <div className="info-content">
             <i className="icons fa-solid fa-location-dot"></i>
             <h5>Maharashtra, India</h5>
           </div>
         </div>
 
-        <div className='links'>
-          <a href="https://www.linkedin.com/in/tejas-khile-b0a45025a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i class="fa-brands fa-linkedin"></i></a>
-          <a href="https://github.com/tejaskhile"><i class="fa-brands fa-github"></i></a>
-          <a href="https://t.me/TejasKhile"><i class="fa-brands fa-telegram"></i></a>
+        <div className="links">
+          <a href="https://www.linkedin.com/in/tejas-khile-b0a45025a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/tejaskhile">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="https://t.me/TejasKhile">
+            <i class="fa-brands fa-telegram"></i>
+          </a>
         </div>
 
-
         {/* Resume */}
-       <div></div>
-        {/* Send a message */}
+        <div className="resume">
+          <button className="resume-btn" onClick={() => window.open("/resume1.pdf", "_blank")}>
+            View Resume
+          </button>
+        </div>
 
         <div className="third-section">
-          <div className='sections'>
-              <li><NavLink to="/about"  
-              className={({isActive}) => location.pathname === '/' || isActive ? 'active' : ''}
-              end>About</NavLink></li>
-              <li><NavLink to="/skills" >Skills</NavLink></li>
-              <li><NavLink to="/projects" >Projects</NavLink></li>
-              <li><NavLink to="/blogs" >Blogs</NavLink></li>
+          <div className="sections">
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  location.pathname === "/" || isActive ? "active" : ""
+                }
+                end
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/skills">Skills</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blogs">Blogs</NavLink>
+            </li>
           </div>
-          <div>
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
