011ebec215fa3e768ec17c630a7920ccb5ecfee5
Tejas Khile <tejaskhile07@gmail.com>
2025-09-10 16:37:26 +0530

    Update project.js

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -37,21 +37,21 @@ const allprojects = [
   {
     name: "Codepen-project",
     caption:
-      "Built a CodePen clone with React! Live HTML, CSS, and JS editing with real-time preview. Perfecting the art of interactive coding environments.<br/> #ReactJS #WebDevelopment",
+      "Built a CodePen clone with React! Live HTML, CSS, and JS editing with real-time preview. Perfecting the art of interactive coding environments.",
     link: "https://tejaskhile.github.io/codepen-clone/",
     date: "Dec 2023",
   },
   {
     name: "Tic Tac Toe",
     caption:
-      "This application effectively demonstrates React's capabilities and offers an interactive, responsive user experience. #WebDevelopment",
+      "This application effectively demonstrates React's capabilities and offers an interactive, responsive user experience.",
     link: "https://tic-tac-toe-one-delta.vercel.app/",
     date: "Oct 2023",
   },
   {
     name: "Music Player",
     caption:
-      "This music player was created using HTML, CSS and JavaScript. The music player features a modern design, with a user-friendly interface that allows users to easily control their music. #WebDevelopment",
+      "This music player was created using HTML, CSS and JavaScript. The music player features a modern design, with a user-friendly interface that allows users to easily control their music.",
     link: "https://music-player-gray-seven.vercel.app/",
     date: "Oct 2023",
   },
